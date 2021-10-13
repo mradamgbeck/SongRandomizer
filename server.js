86f13eaaecc412c97db8f09345e1a056bea90fd4
mradamgbeck <mradamgbeck@gmail.com>
2021-10-13 16:13:53 -0400

    now serving with express

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const express = require('express');
-const {generateSong} = require('./src/lib/generate')
-const app = express();
-app.use(express.json());
-
-app.post('/generate', function (req, res) {
-    let song = generateSong(req.body);
-    res.send(song);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log('song randomizer listening on port ' + port);
-});
+const serveStatic = require("serve-static")
+const path = require('path');
+let app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
